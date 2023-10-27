53cf6d504b3e53f98ce6cc16e90728a2ce5fe61f
Humayun <mumtazhumayun6@gmail.com>
2023-10-27 22:47:30 +0500

    added files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue': '#1c2434',
+        'sidebar' : '#333a48'
+      },
+    },
+  },
+  plugins: [],
+}
+
